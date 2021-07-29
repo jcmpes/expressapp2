a7e662a5afb4cb930c8e73aad4d2b350b5a2badf
JC <jcmpes@gmail.com>
2021-07-29 11:23:16 +0200

    CICD test env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = 3005
-const IP_SERVER = process.env.IP_SERVER
+const IP_SERVER = process.env.IP_SERVER;
 
 app.get('/', (req, res) => {
   res.send(`Hello World from ${IP_SERVER}!`)
