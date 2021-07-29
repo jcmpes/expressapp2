9eaa239ac906fcce63132f9c6a3d453ac0445dfa
JC <jcmpes@gmail.com>
2021-07-29 16:47:29 +0200

    CICD: testing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const IP_SERVER = process.env.IP_SERVER;
 
 app.get('/', (req, res) => {
   res.send(`Hello World from ${IP_SERVER}!`)
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
