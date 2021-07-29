078edaf68db7d5882af60a9e67df490f35862113
JC <jcmpes@gmail.com>
2021-07-29 15:07:27 +0200

    CICD new image

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const port = 3005
 const IP_SERVER = process.env.IP_SERVER;
 
 app.get('/', (req, res) => {
-  res.send(`Hello World from ${IP_SERVER}! Los cambios se despliegan solos 😀`)
+  // res.send(`Hello World from ${IP_SERVER}! Los cambios se despliegan solos 😀`)
+  res.set('Content-Type', 'text/html')
+  res.send(Buffer.from('<html><body><img src="https://www.uncsa.edu/mysa/img/announcements/2021/trophy-award-confetti.jpg"></body></html>'))
 })
 
 app.listen(port, () => {
